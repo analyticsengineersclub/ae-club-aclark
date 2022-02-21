deebe3b4773dd38a6182ad9e3e9be4e56a653982
Ubuntu <ubuntu@ip-172-31-30-58.us-east-2.compute.internal>
2022-02-21 19:11:04 +0000

    add customers query

---
+++ customers.sql
@@ -0,0 +1,14 @@
+/* For each customer, we want to know how many orders they have placed, and when they placed their first order. */
+
+SELECT 
+   c.id AS customer_id
+   , c.name
+   , c.email
+   , COUNT(o.id) AS total_orders
+   , MIN (o.created_at) AS first_order_at
+FROM analytics-engineers-club.coffee_shop.orders AS o
+LEFT JOIN analytics-engineers-club.coffee_shop.customers AS c
+  ON o.customer_id = c.id
+GROUP BY 1,2,3
+ORDER BY first_order_at 
+LIMIT 5
